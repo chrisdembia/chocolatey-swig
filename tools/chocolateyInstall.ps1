e6d785326f38971d1875850aad63ac167265015d
Christopher Dembia <cld72@cornell.edu>
2017-06-09 22:47:50 -0700

    Prep resubmission for 3.0.8.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'swig'
 $url = "http://downloads.sourceforge.net/project/swig/swigwin/swigwin-$version/swigwin-$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version = '3.0.12'
+$version = '3.0.8'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
