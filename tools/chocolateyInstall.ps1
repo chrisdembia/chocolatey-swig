b14633906ecaa0febee4c95f4be93416495f228b
Daniel Wozniak <dan@woz.io>
2018-02-27 01:51:40 -0700

    Use ssl version of sourceforge url (avoid 112)
    Avoid issues with newer chocolaty versions
    https://github.com/chocolatey/choco/issues/112#issue-58511395

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'swig'
-$url = "http://downloads.sourceforge.net/project/swig/swigwin/swigwin-$version/swigwin-$version.zip"
+$url = "https://downloads.sourceforge.net/project/swig/swigwin/swigwin-$version/swigwin-$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version = '3.0.12'
 
